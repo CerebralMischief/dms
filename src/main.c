03508d5c0cd66f3a9f0b1a622328e9c0c09888be
Cerebral Mischief <ericsb@live.com>
2017-12-28 13:02:53 -0500

    Update main.c
    Updated comments.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
-// my first program in C++
 #include <iostream>
 
 int main()
 {
+  /* TODO: Implement Dead Man's Switch functionality:
+       1) Record the last time a given user interacted with DMS.
+         a) Once the configured time out elapses react by triggering the OnKill event
+       2) Allow interaction to update the last time a given user interacted.
+  */
+  
   std::cout << "Hello World!";
 }
